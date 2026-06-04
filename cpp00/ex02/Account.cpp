daa3f061ab17ef3e862895620df3ac0878dc3592
Ali Bazzoun <abazzoun@c2r7s2.42beirut.com>
2026-06-04 17:58:19 +0300

    ex02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,6 +1,33 @@
 #include "Account.hpp"
 
+Account::Account(int initial_deposit)
+{
+	_amount = initial_deposit;
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
 
-static void	Account::_displayTimestamp( void ) {
+	std::cout << "index:"	<< _accountIndex  << ";";
+	std::cout << "amount:"	<< _amount		  << ";";
+	std::cout << "created"	<< std::endl;
+}
+
+static int	Account::getNbAccounts()	{ return _nbAccounts; }
+static int	Account::getTotalAmount()	{ return _totalAmount; }
+static int	Account::getNbDeposits()	{ return _totalNbDeposits; }
+static int	Account::getNbWithdrawals()	{ return _totalNbWithdrawals; }
+
+static void Account::displayAccountsInfos( void  )
+{
+	if (_totalAmount == 0)
+		return ;
+}
+
+void Account::displayStatus( void )
+{
+}
+
+static void	Account::_displayTimestamp( void )
+{
 	std::cout << "[" << 
 }
